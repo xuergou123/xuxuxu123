3414c59dd00cdfe5ea3ffbd2f9e506054725d0cf
xuergou123 <1498390546@qq.com>
2019-02-22 16:06:45 +0800

    新增和编辑商品

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {post,fetch,patch,put} from './http'
+import { post, fetch, patch, put } from './http'
 //定义全局变量
 // Vue.prototype.$post=post;
 // Vue.prototype.$fetch=fetch;
@@ -9,7 +9,7 @@ import {post,fetch,patch,put} from './http'
 
 //export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
-export const requestLogin = params => { debugger;return this.$fetch(`/dasa/login`, params).then(res => res.data); };
+export const requestLogin = params => { debugger; return this.$fetch(`/dasa/login`, params).then(res => res.data); };
 
 
 export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
@@ -23,3 +23,12 @@ export const batchRemoveUser = params => { return axios.get(`${base}/user/batchr
 export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
 
 export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+
+
+export const genUuid = params => {
+
+    function S4() {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    }
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
